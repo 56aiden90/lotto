5cf07901cc2c9aef06b712bfc86612b88cf3f4ce
Aiden Jung <aiden.jung@harqen.com>
2020-12-12 13:41:13 +0900

    cleaning up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Button } from "antd";
-import { AppContext } from "@lib/context";
 import { useContext } from "react";
 
 const Title = styled.h1`
@@ -9,14 +8,9 @@ const Title = styled.h1`
 `;
 
 export default function Home() {
-  const { appMessage } = useContext(AppContext);
-
   return (
     <div style={{ marginTop: 100 }}>
       <Title>스타일드 컴포넌트 작동예시</Title>
-      <Button onClick={() => appMessage.info("안녕하세요?")}>
-        컨텍스트 및 antd 테스트
-      </Button>
     </div>
   );
 }
