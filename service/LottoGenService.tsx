6280a1deedab8b167f0ab685de75de4c461811bc
JeGwan <emperorvv@gmail.com>
2020-12-26 20:16:19 +0900

    prettier config 추가

--- service/LottoGenService.tsx
+++ service/LottoGenService.tsx
@@ -1,7 +1,16 @@
-import ApiService from './ApiService';
-import { Moment } from 'moment';
+import ApiService from "./ApiService";
+import { Moment } from "moment";
+
+const a = {
+    aabb: 123,
+    b: 23,
+};
 
 class LottoGenService extends ApiService {
+    constructor() {
+        super();
+        console.log("LottoGenService created.");
+    }
     genNumbersByQuote(quote: string): number[] | PromiseLike<number[]> {
         // return new Promise(resolve => resolve([1, 2, 3, 4, 5, 6]));
         if (quote == null) {
@@ -9,12 +18,17 @@ class LottoGenService extends ApiService {
         }
         return this.getJson("/main?string=" + quote);
     }
-    genNumbersByBirthDate(birthDate: Moment | null, name: string | null): Promise<any> {
+    genNumbersByBirthDate(
+        birthDate: Moment | null,
+        name: string | null,
+    ): Promise<any> {
         // return new Promise(resolve => resolve([1, 2, 3, 4, 5, 6]));
         if (birthDate == null) {
             throw new Error("Invalid birthDate");
         }
-        return this.getJson("/main?string=" + birthDate.format("YYYY-MM-DD") + name);
+        return this.getJson(
+            "/main?string=" + birthDate.format("YYYY-MM-DD") + name,
+        );
     }
 }
 
