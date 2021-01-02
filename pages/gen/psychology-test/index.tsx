705d9eb81e9a331cb22a1ea0e2b5a281058e020f
Aiden Jung <xenosophian@gmail.com>
2021-01-02 16:39:36 +0900

    1. Added logic to generate numbers by psyTest 2. Styling result page

--- pages/gen/psychology-test/index.tsx
+++ pages/gen/psychology-test/index.tsx
@@ -15,6 +15,7 @@ const Wrapper = styled.div`
     }
     .sectionName {
         font-size: 2.5rem;
+        margin-bottom : 0;
     }
     .form {
         display: flex;
@@ -25,6 +26,16 @@ const Wrapper = styled.div`
         width: 100%;
         margin-bottom: 4px;
         max-width: 300px;
+    }
+    .questionWrapper{
+        display : flex;
+        flex-direction : column;
+        justify-content : center;
+        text-align : center;
+        .question-title{
+            margin : 10px 0;
+            font-size : 1.3rem;
+        }
     }
     .question {
         display: flex;
@@ -42,23 +53,28 @@ const Wrapper = styled.div`
         opacity: 0;
         transition: all 0.3s ease-in-out;
     }
-    .navigate-wrapper {
+    .navWrapper {
         margin-top: 10px;
         display: flex;
         justify-content: space-around;
         > div {
             width: 50px;
         }
-        .nav-btn {
+        .navBtn {
             text-align: center;
             border-radius: 25px;
             background-color: rgb(46, 86, 179);
             color: white;
-            padding: 5px 0;
+            padding: 5px 5px;
+            width : 80px;
+            white-space : nowrap;
             font-weight: 400;
             span {
                 position: relative;
                 top: 1px;
+            }
+            &:hover{
+                cursor : pointer;
             }
         }
     }
@@ -69,7 +85,7 @@ const Wrapper = styled.div`
 const mockTest: Question[] = [
     {
         id: 1,
-        title: "가장 좋아하는 색깔이 뭐니?",
+        title: "가장 좋아하는 색깔을 골라주세요",
         options: [
             {
                 label: "빨강",
@@ -91,59 +107,119 @@ const mockTest: Question[] = [
     },
     {
         id: 2,
-        title: "가장 좋아하는 음계는 뭐니",
+        title: "다시 태어난다면 어느 대륙 사람으로 태어나고 싶으신가요?",
         options: [
             {
-                label: "도",
-                value: "do",
+                label: "아시아",
+                value: "asia",
             },
             {
-                label: "레",
-                value: "le",
+                label: "북미",
+                value: "northAmerica",
             },
             {
-                label: "미",
-                value: "mi",
+                label: "남미",
+                value: "southAmerica",
             },
             {
-                label: "파",
-                value: "pa",
+                label: "유럽",
+                value: "europe",
+            },
+            {
+                label: "아프리카",
+                value: "africa",
             },
         ],
     },
     {
         id: 3,
-        title: "너가 전생에 핸드폰이라면 넌 무슨 기종일까",
+        title: "오늘의 기분을 5점 만점 점수로 표현한다면?",
         options: [
             {
-                label: "아이폰3",
-                value: "iphone3",
+                label: "1점",
+                value: "1",
             },
             {
-                label: "갤럭시노트7",
-                value: "galaxyNote7",
+                label: "2점",
+                value: "2",
             },
             {
-                label: "블랙베리",
-                value: "blackberry",
+                label: "3점",
+                value: "3",
             },
             {
-                label: "픽셀4a",
-                value: "pixel4a",
+                label: "4점",
+                value: "4",
             },
+            {
+                label: "5점",
+                value: "5",
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: "가장 좋아하는 계절을 골라주세요",
+        options: [
+            {
+                label: "봄",
+                value: "spring",
+            },
+            {
+                label: "여름",
+                value: "summer",
+            },
+            {
+                label: "가을",
+                value: "fall",
+            },
+            {
+                label: "겨울",
+                value: "winter",
+            }
+        ],
+    },
+    {
+        id: 5,
+        title: "다음 중 가장 마음에 드는 단어를 골라주세요",
+        options: [
+            {
+                label: "갱생",
+                value: "rebirth",
+            },
+            {
+                label: "성공",
+                value: "success",
+            },
+            {
+                label: "권력",
+                value: "power",
+            },
+            {
+                label: "부",
+                value: "wealth",
+            }
         ],
     },
 ];
 
 export default function Home() {
-    const [name, setName] = useState<string>("");
     const router = useRouter();
     const [psyTest, setPsyTest] = useState(mockTest);
     const [questionId, setQuestionId] = useState(psyTest[0].id);
     const [opacity, setOpacity] = useState(true);
     const submitForm = async () => {
         try {
-        } catch (err) {}
+            // const quote = psyTest.reduce((prev, cur)=>prev + cur?.selected?.toString(), "");
+            // const generatedNumbers: number[] = await LottoGenService.genNumbersByQuote(quote);
+            const generatedNumbers = [1,2,3,4,5,6];
+            router.push({
+                pathname: '/result',
+                query: { generatedNumbers },
+            })
+        } catch (err) {
+            console.log(err);
+        }
     };
 
     const renderQuestion = () => {
@@ -162,7 +238,8 @@ export default function Home() {
                 );
             };
             return (
-                <div className="question-wrapper">
+                <div className="questionWrapper">
+                    <p className="question-title">{question.title}</p>
                     <Radio.Group
                         className={`question ${opacity ? "show" : "hide"}`}
                         options={question.options}
@@ -170,8 +247,9 @@ export default function Home() {
                         value={question.selected}
                         optionType="button"
                         buttonStyle="solid"
+                        size="large"
                     />
-                    <div className="navigate-wrapper">
+                    <div className="navWrapper">
                         {questionId === psyTest[0].id ? (
                             <div />
                         ) : (
@@ -183,23 +261,31 @@ export default function Home() {
                                         setOpacity(true);
                                     }, 0);
                                 }}
-                                className="previous-btn nav-btn"
+                                className="prevBtn navBtn"
                             >
                                 <span>이전</span>
                             </div>
                         )}
                         {questionId === psyTest[psyTest.length - 1].id ? (
-                            <div />
+                            <div
+                            className="nextBtn navBtn"
+                            onClick={submitForm}>
+                                <span>결과 보기</span>
+                            </div>
                         ) : (
                             <div
                                 onClick={() => {
+                                    if(!question.selected || typeof question.selected === 'undefined'){
+                                        alert("답변을 골라주세요.")
+                                        return;
+                                    }
                                     setOpacity(false);
                                     setQuestionId(questionId + 1);
                                     setTimeout(() => {
                                         setOpacity(true);
                                     }, 0);
                                 }}
-                                className="next-btn nav-btn"
+                                className="nextBtn navBtn"
                             >
                                 <span>다음</span>
                             </div>
