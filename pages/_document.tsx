5bdf4d3644f720ab0075b70652f686745b76373f
JeGwan <emperorvv@gmail.com>
2021-01-16 12:26:08 +0900

    favicon social 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -46,6 +46,48 @@ export default class MyDocument extends Document {
                     ></link>
                     {/* three.js 여기에 두는게 최선? */}
                     {/* <script src="https://threejs.org/build/three.min.js"></script> */}
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/favicon/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/favicon/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/favicon/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="/favicon/site.webmanifest" />
+                    <link
+                        rel="mask-icon"
+                        href="/favicon/safari-pinned-tab.svg"
+                        color="#5bbad5"
+                    />
+                    <link rel="shortcut icon" href="/favicon/favicon.ico" />
+                    <meta name="msapplication-TileColor" content="#ffffff" />
+                    <meta
+                        name="msapplication-config"
+                        content="/favicon/browserconfig.xml"
+                    />
+                    <meta name="theme-color" content="#ffffff" />
+                    <meta property="og:image" content="/social/og-image.jpg" />
+                    <meta property="og:image:height" content="209" />
+                    <meta property="og:image:width" content="400" />
+                    <meta
+                        property="og:url"
+                        content="https://lotto.superposition.link"
+                    />
+                    <meta
+                        property="og:description"
+                        content="생년월일 / 글귀 / 심리테스트로 로또 번호를 만들어 보자"
+                    />
+                    <meta property="og:title" content="육성장군" />
                 </Head>
                 <body>
                     <Main />
