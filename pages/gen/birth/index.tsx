0137c2bc59ccfb19a152325b4e4676237d47cb38
JeGwan <emperorvv@gmail.com>
2021-01-16 11:05:50 +0900

    birth 업데이트

--- pages/gen/birth/index.tsx
+++ pages/gen/birth/index.tsx
@@ -1,15 +1,12 @@
 import { useContext, useState } from "react";
 import styled from "styled-components";
-import { DatePicker, Button, Input } from "antd";
+import { Button, Input, Select } from "antd";
 import Layout from "@components/Layout";
-import { Moment } from "moment";
-import { locale } from "@lib/locale";
 import { AppContext } from "@lib/context";
 import Axios from "axios";
 import Ball from "@components/Ball";
 import { useRouter } from "next/router";
-import querystring from "querystring";
-import { RESULT_TYPE } from "@lib/enums";
+const { Option } = Select;
 const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
@@ -19,7 +16,19 @@ const Wrapper = styled.div`
         display: block;
         width: 100%;
         margin-bottom: 4px;
-        max-width: 300px;
+        max-width: 400px;
+    }
+    .row {
+        display: flex;
+        align-items: center;
+        & > * {
+            text-align: left;
+            flex: 1 0 0;
+            margin-right: 4px;
+            &:last-child {
+                margin-right: 0;
+            }
+        }
     }
     .name {
         font-size: 20px;
@@ -29,7 +38,7 @@ const Wrapper = styled.div`
     }
     .sectionName {
         font-size: 2.5rem;
-        margin-bottom : 0;
+        margin-bottom: 0;
     }
 `;
 
@@ -37,39 +46,34 @@ const Birth = () => {
     const router = useRouter();
     const { appMessage } = useContext(AppContext);
     const [name, setName] = useState("");
-    const [birthDate, setBirthDate] = useState<Moment | null>(null);
+
+    const [year, setYear] = useState<number>();
+    const [month, setMonth] = useState<number>();
+    const [date, setDate] = useState<number>();
+
     const [loading, setLoading] = useState(false);
     const [numbers, setNumbers] = useState<number[]>();
     const genLottoNumber = () => {
         if (name.length < 2)
             return appMessage.warn("이름은 2글자 이상 입력해주세요.");
-        if (birthDate === null)
-            return appMessage.warn("생년월일을 선택해주세요.");
-        // setLoading(true);
-        const birth = birthDate.format("YYYY-MM-DD");
-        Axios.get(`https://lotto-api.superposition.link/main?string=${encodeURIComponent(name + birth)}`,{
-            headers :{
-                passwd : 'gworld'
-            }
-        })
+        if (!year) return appMessage.warn("년도를 선택해주세요.");
+        if (!month) return appMessage.warn("월을 선택해주세요.");
+        if (!date) return appMessage.warn("일을 선택해주세요.");
+
+        const birth = `${year}-${month}-${date}`;
+        const url = `https://lotto-api.superposition.link/main?string=${encodeURIComponent(
+            name + birth,
+        )}`;
+        Axios.get(url, { headers: { passwd: "gworld" } })
             .then(({ data }) => {
-                    const qs = querystring.stringify({
-                        name,
-                        birth,
-                        type: RESULT_TYPE.BIRTH,
-                        numbers: data.res,
-                    });
-                    console.log(qs);
-                    router.push("/result?" + qs);
-                    setNumbers(data.res);
+                setNumbers(data.res);
             })
             .catch((err) => {
                 appMessage.error("서버 내부 에러");
                 console.error(err);
-            })
-            // .finally(() => setLoading(false));
+            });
+        // .finally(() => setLoading(false));
     };
-
     return (
         <Layout pageTitle="육성장군">
             <Wrapper>
@@ -77,23 +81,22 @@ const Birth = () => {
                 {numbers ? (
                     <>
                         <span className="birth">
-                            {birthDate?.format("YYYY년 MM월 DD일생")}
+                            {/* {birthDate?.format("YYYY년 MM월 DD일생")} */}
                         </span>
                         <span className="name">{name} 님의 번호</span>
                         <div className="numbers">
                             {numbers.map((number, index) => (
                                 <Ball
                                     key={index}
-                                    delay={index * 400}
+                                    delay={300 + index * 100}
                                     number={number}
-                                />
+                                ></Ball>
                             ))}
                         </div>
                         <Button
                             size="large"
                             onClick={() => {
-                                setBirthDate(null);
-                                setName("");
+                                // setBirthDate(null);
                                 setNumbers(undefined);
                             }}
                             type="primary"
@@ -102,31 +105,64 @@ const Birth = () => {
                         </Button>
                     </>
                 ) : (
-                        <>
+                    <>
+                        <Input
+                            size="large"
+                            placeholder="이름"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            disabled={loading}
+                        />
+                        <div className="row">
                             <Input
+                                className="year"
                                 size="large"
-                                placeholder="이름"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
+                                placeholder="년도"
+                                value={year}
+                                onChange={(e) => {
+                                    let y = +e.target.value;
+                                    if (Number.isNaN(y)) y = 0;
+                                    setYear(y);
+                                }}
                                 disabled={loading}
                             />
-                            <DatePicker
+                            <Select
+                                className="month"
                                 size="large"
-                                locale={locale}
-                                onChange={(value) => setBirthDate(value)}
-                                placeholder="생년월일 선택"
-                                disabled={loading}
-                            />
-                            <Button
-                                size="large"
-                                type="primary"
-                                onClick={genLottoNumber}
-                                loading={loading}
+                                placeholder="월"
+                                value={month}
+                                onChange={(value) => setMonth(value)}
                             >
-                                번호 생성
+                                {new Array(12).fill(0).map((v, i) => (
+                                    <Option value={i + 1} key={i + 1}>
+                                        {i + 1}월
+                                    </Option>
+                                ))}
+                            </Select>
+                            <Select
+                                className="date"
+                                size="large"
+                                placeholder="일"
+                                value={date}
+                                onChange={(value) => setDate(value)}
+                            >
+                                {new Array(31).fill(0).map((v, i) => (
+                                    <Option value={i + 1} key={i + 1}>
+                                        {i + 1}일
+                                    </Option>
+                                ))}
+                            </Select>
+                        </div>
+                        <Button
+                            size="large"
+                            type="primary"
+                            onClick={genLottoNumber}
+                            loading={loading}
+                        >
+                            번호 생성
                         </Button>
-                        </>
-                    )}
+                    </>
+                )}
             </Wrapper>
         </Layout>
     );
